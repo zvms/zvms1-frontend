26cc19395a00861bc6d559f965ccb3dc2267ffe7
Zecyel <zhuchengyangclever@163.com>
2021-08-30 17:46:25 +0800

    add some utility

--- src/views/volunteer/audit.vue
+++ src/views/volunteer/audit.vue
@@ -28,101 +28,101 @@
     </v-card>
     <v-dialog v-model="dialog1" max-width="80%">
       <v-card>
-		<v-card-title>详细信息</v-card-title>
+        <v-card-title>详细信息</v-card-title>
         <v-simple-table style="margin:20px;">
           <tbody>
-			<tr>
-			  <td>义工编号</td>
-			  <td>{{volid}}</td>
-			</tr>
-			<tr>
-			  <td>义工日期</td>
-			  <td>{{volDate}}</td>
-			</tr>
-			<tr>
-			  <td>义工时间</td>
-			  <td>{{volTime}}</td>
-			</tr>
-			<tr>
-			  <td>义工详细信息</td>
-			  <td>{{volDesc}}</td>
-			</tr>
-			<tr>
-			  <td>校内时长</td>
-			  <td>{{volTI}}</td>
-			</tr>
-			<tr>
-			  <td>校外时长</td>
-			  <td>{{volTO}}</td>
-			</tr>
-			<tr>
-			  <td>大型时长</td>
-			  <td>{{volTL}}</td>
-			</tr>
-			<tr>
-			  <td>学号</td>
-			  <td>{{stuid}}</td>
-			</tr>
-			<tr>
-			  <td>感想</td>
-			  <td>{{thought}}</td>
-			</tr>
-			<tr>
-			  <td>发放的校内时长</td>
-			  <td><v-text-field
-			    v-model="inside"
+            <tr>
+              <td>义工编号</td>
+              <td>{{volid}}</td>
+            </tr>
+            <tr>
+              <td>义工日期</td>
+              <td>{{volDate}}</td>
+            </tr>
+            <tr>
+              <td>义工时间</td>
+              <td>{{volTime}}</td>
+            </tr>
+            <tr>
+              <td>义工详细信息</td>
+              <td>{{volDesc}}</td>
+            </tr>
+            <tr>
+              <td>校内时长</td>
+              <td>{{volTI}}</td>
+            </tr>
+            <tr>
+              <td>校外时长</td>
+              <td>{{volTO}}</td>
+            </tr>
+            <tr>
+              <td>大型时长</td>
+              <td>{{volTL}}</td>
+            </tr>
+            <tr>
+              <td>学号</td>
+              <td>{{stuid}}</td>
+            </tr>
+            <tr>
+              <td>感想</td>
+              <td>{{thought}}</td>
+            </tr>
+            <tr>
+              <td>发放的校内时长</td>
+              <td><v-text-field
+                v-model="inside"
                 label="不填为默认值"
                 prepend-icon="mdi-view-list"
               /></td>
-			</tr>
-			<tr>
-			  <td>发放的校外时长</td>
-			  <td><v-text-field
-			    v-model="outside"
+            </tr>
+            <tr>
+              <td>发放的校外时长</td>
+              <td><v-text-field
+                v-model="outside"
                 label="不填为默认值"
                 prepend-icon="mdi-view-list"
               /></td>
-			</tr>
-			<tr>
-			  <td>发放的大型时长</td>
-			  <td><v-text-field
-			    v-model="large"
+            </tr>
+            <tr>
+              <td>发放的大型时长</td>
+              <td><v-text-field
+                v-model="large"
                 label="不填为默认值"
                 prepend-icon="mdi-view-list"
               /></td>
-			</tr>
+            </tr>
           </tbody>
         </v-simple-table>
         <v-card-actions>
           <v-spacer></v-spacer>
-		  <v-btn
+          <v-btn
             color="primary"
             block
             :disabled="$store.state.isLoading"
             @click="audit(1)"
           >通过
-		  </v-btn>
-		  <v-btn
+          </v-btn>
+          <v-btn
             color="primary"
             block
             :disabled="$store.state.isLoading"
             @click="audit(2)"
           >拒绝
-		  </v-btn>
-		  <v-btn
+          </v-btn>
+          <v-btn
             color="primary"
             block
             :disabled="$store.state.isLoading"
             @click="audit(3)"
           >打回
-		  </v-btn>
-		  <v-btn
+          </v-btn>
+          <v-btn
             color="primary"
             block
             :disabled="$store.state.isLoading"
             @click="dialog1 = false"
           >取消
-		  </v-btn>
+          </v-btn>
         </v-card-actions>
       </v-card>
     </v-dialog>
@@ -144,19 +144,19 @@ export default {
       { text: "学号", value: "stuId" },
     ],
     thoughts: undefined,
-	  dialog1: false,
-	  stuid: undefined,
-	  volid: undefined,
-	  thought: undefined,
-	  volTime: undefined,
-	  volDate: undefined,
-	  volDesc: undefined,
-	  volTI: undefined,
-	  volTO: undefined,
-	  volTL: undefined,
-	  inside: undefined,
-	  outside: undefined,
-	  large: undefined
+    dialog1: false,
+    stuid: undefined,
+    volid: undefined,
+    thought: undefined,
+    volTime: undefined,
+    volDate: undefined,
+    volDesc: undefined,
+    volTI: undefined,
+    volTO: undefined,
+    volTL: undefined,
+    inside: undefined,
+    outside: undefined,
+    large: undefined
   }),
   mounted: function () {
     this.pageload();
@@ -169,13 +169,13 @@ export default {
 
         })
         .then((response) => {
-            console.log(response.data);
-            if (response.data.type == "SUCCESS") {
-              dialogs.toasts.success(response.data.message);
-              this.thoughts = response.data.result;
-            } else {
-              dialogs.toasts.error(response.data.message);
-            }
+          console.log(response.data);
+          if (response.data.type == "SUCCESS") {
+            dialogs.toasts.success(response.data.message);
+            this.thoughts = response.data.result;
+          } else {
+            dialogs.toasts.error(response.data.message);
+          }
         })
         .catch((err) => {
           dialogs.toasts.error(err);
@@ -188,80 +188,80 @@ export default {
     granted: function () {
       return this.$store.state.info.permission < permissions.teacher;
     },
-	rowClick: function (item) {
-	  this.dialog1 = true;
-	  this.volid = item.volId;
-	  this.stuid = item.stuId;
-	  this.thought = item.thought;
-	  
-	  this.$store.commit("loading", true);
-    axios
-      .get("/volunteer/fetch/"+this.volid,{
-
-      })
-      .then((response) => {
+    rowClick: function (item) {
+      this.dialog1 = true;
+      this.volid = item.volId;
+      this.stuid = item.stuId;
+      this.thought = item.thought;
+      
+      this.$store.commit("loading", true);
+      axios
+        .get("/volunteer/fetch/"+this.volid,{
+      
+        })
+        .then((response) => {
           console.log(response.data);
           if (response.data.type == "SUCCESS") {
             dialogs.toasts.success(response.data.message);
             this.volDate = response.data.date;
             this.volTime = response.data.time;
             this.volDesc = response.data.description;
-		        this.volTI = response.data.inside;
-		        this.volTO = response.data.outside;
-		        this.volTL = response.data.large;
+            this.volTI = response.data.inside;
+            this.volTO = response.data.outside;
+            this.volTL = response.data.large;
           } else {
             dialogs.toasts.error(response.data.message);
           }
-      })
-      .catch((err) => {
-        dialogs.toasts.error(err);
-      })
-      .finally(() => {
-        this.$store.commit("loading", false);
-      });
-    this.$store.commit("loading", false);
-	},
-	audit: function (status) {
-    if(this.inside==undefined || this.inside=="")
-      this.inside = this.volTI;
-    if(this.outside==undefined || this.outside=="")
-      this.outside = this.volTO;
-    if(this.large==undefined || this.large=="")
-      this.large = this.volTL;
-    console.log(status,this.inside,this.outside,this.large);
-	  this.$store.commit("loading", true);
-    axios
-      .post("/volunteer/audit/"+this.volid,{
-        "thought": [{
-          "stuId": this.stuid,
-          "status": status,
-          "inside": this.inside,
-          "outside": this.outside,
-          "large": this.large
-        }]
-      })
-      .then((response) => {
-          console.log(response.data);
-          if (response.data.type == "SUCCESS") {
-            dialogs.toasts.success(response.data.message);
-            this.volDate = response.data.date;
-            this.volTime = response.data.time;
-            this.volDesc = response.data.description;
-		        this.volTI = response.data.inside;
-		        this.volTO = response.data.outside;
-		        this.volTL = response.data.large;
-          } else {
-            dialogs.toasts.error(response.data.message);
-          }
-      })
-      .catch((err) => {
-        dialogs.toasts.error(err);
-      })
-      .finally(() => {
-        this.$store.commit("loading", false);
-      });
-    this.$store.commit("loading", false);
-	}
+        })
+        .catch((err) => {
+          dialogs.toasts.error(err);
+        })
+        .finally(() => {
+          this.$store.commit("loading", false);
+        });
+      this.$store.commit("loading", false);
+    },
+    audit: function (status) {
+      if(this.inside==undefined || this.inside=="")
+        this.inside = this.volTI;
+      if(this.outside==undefined || this.outside=="")
+        this.outside = this.volTO;
+      if(this.large==undefined || this.large=="")
+        this.large = this.volTL;
+      console.log(status,this.inside,this.outside,this.large);
+        this.$store.commit("loading", true);
+      axios
+        .post("/volunteer/audit/"+this.volid,{
+          "thought": [{
+            "stuId": this.stuid,
+            "status": status,
+            "inside": this.inside,
+            "outside": this.outside,
+            "large": this.large
+          }]
+        })
+        .then((response) => {
+            console.log(response.data);
+            if (response.data.type == "SUCCESS") {
+              dialogs.toasts.success(response.data.message);
+              this.volDate = response.data.date;
+              this.volTime = response.data.time;
+              this.volDesc = response.data.description;
+              this.volTI = response.data.inside;
+              this.volTO = response.data.outside;
+              this.volTL = response.data.large;
+            } else {
+              dialogs.toasts.error(response.data.message);
+            }
+        })
+        .catch((err) => {
+          dialogs.toasts.error(err);
+        })
+        .finally(() => {
+          this.$store.commit("loading", false);
+        });
+      this.$store.commit("loading", false);
+    }
   },
 };
 </script>
