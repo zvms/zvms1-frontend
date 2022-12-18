3460cce457eac4d2b9f457231c7430250153d100
fxn5rwspnocvr1nb <cqs6nn1o39@163.com>
2022-12-18 09:06:14 +0800

    哼哼哼啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊

--- shell/users.py
+++ shell/users.py
@@ -1,11 +1,18 @@
 from shell import App
+from req import get, post, headers
 
 users = App('users', '用户管理:')
 
 @users.route('user login <int:id> <pwd>')
 def login(id, pwd):
-    print('login', id, pwd)
+    res = post('/users/login', id=id, pwd=pwd)
+    if res:
+        headers['Authorization'] = res['token']
 
 @users.route('user logout')
 def logout():
-    print('logout')
+    post('/users/logout')
+
+@users.route('user <int:id>')
+def get_user_info(id):
+    print(get(f'/users/{id}'))
